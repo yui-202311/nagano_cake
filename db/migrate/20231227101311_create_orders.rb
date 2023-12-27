ee6425bc76d546b654e8a3248c612787b2d5f4c3
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-27 10:36:25 +0000

    Add model-Item, CartItem, Order, OrderDetail

---
+++ db/migrate/20231227101311_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      
+      t.integer :customer_id, null: false
+      t.string :shipping_postal_code, null: false
+      t.string :shipping_address, null: false
+      t.string :shipping_name, null: false
+      t.integer :payment_amount, null: false
+      t.integer :payment_method, null: false
+      t.integer :postage, null: false
+      
+      t.timestamps
+    end
+  end
+end
