53943fe5025418bf7a7f8f737060e68e11115945
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-28 23:45:42 +0000

    next make 遷移先

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
+  root to: 'home#top'
+  get "home/about"=> "home#about", as: "about"
+  
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users
+
+  # resources public, only: [:create, :index, :show, :edit, :update, :destroy]
+  # resources :admin, only: [:create, :new, :destroy]
+
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20231227110713_devise_create_admins.rb
+++ db/migrate/20231227110713_devise_create_admins.rb
@@ -13,9 +13,6 @@ class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
 
       ## Rememberable
       t.datetime :remember_created_at
-      t.string :email, null: false
-      t.string :encrypted_password, null: false
-
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
