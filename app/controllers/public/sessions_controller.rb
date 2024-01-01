e1043d1fc90bd7b0802f40e99da92a0bac426fff
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2024-01-01 12:40:11 +0000

    continue

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -13,6 +13,22 @@ def after_sign_in_path_for(resource)
     root_path
   end
 end
+
+before_action customer_state if: :is_active
+@customer = current.user
+
+private
+def customer_state
+  if @customer.is_active == true
+  customer = Customer.find_by(email: params[:customer][:email])
+  elsif @customer.is_active :false
+    redirect_to action: 'create'
+  return if customer.nil?
+  @customer.password = params[:password]
+  return unless customer.valid_password?(params[:customer][:password])
+  redirect_to sign_up_path
+  end
+end
   # GET /resource/sign_in
   # def new
   #   super
