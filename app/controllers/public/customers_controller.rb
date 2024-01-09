f758c46ede222f9f0600dfe09a8c0ccc7fe69c67
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2024-01-09 15:05:49 +0000

    NEXT　明日までの期限で設定している者たちの作業

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,6 @@
 class Public::CustomersController < ApplicationController
   def show
-     @customer = Customer.find(params[:id])
+     @customer = current_customer
   end
 
   def edit
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
-    @item = Item.new
+    @total_item = Item.count
   end
 
   def show
