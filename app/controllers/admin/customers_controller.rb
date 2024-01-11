932636c5b572d6121c8a26cf7329d5a288045cef
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2024-01-11 13:32:35 +0000

    NEXT カート

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,9 +8,21 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
-    
+     @customer = Customer.find(current_customer.id)
   end
 
   def update
+      @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path
+    else
+      @customers = current_customer
+      render :edit
+    end
+  end
+  
+  def customer_params
+  params.require(:customer).permit(:email, :first_name, :last_name, :first_name_kana, :first_name_kana,
+                                  :postal_code, :address, :telephone_number)
   end
 end
