ea1a8f308c6213b99fc3f12548b37ca8c2981014
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2024-01-14 09:58:26 +0000

    変更ボタン修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,20 +1,29 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items.all
-    # @cart_item = current_customer.cart_items(cart_item_params)
     @total = 0
   end
 
   def update
+      @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to 
+    else
+      # @cart_items = CartItem.find(params[:id])
+      render :index
+    end
   end
 
   def destroy
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
     redirect_to public_cart_items_path
   end
 
   def destroy_all
+    cart_items = current_customer.cart_items
+    cart_items.destroy_all
+    redirect_to public_items_path
   end
 
   def create
@@ -25,7 +34,7 @@ class Public::CartItemsController < ApplicationController
       render :show
     end
   end
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount, :name)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
     patch "customers/information" => "customers#update"
     get "customers/withdrawal" => "customers#withdrawal"
     patch "customers/unsubscribe" => "customers#unsubscribe"
+    delete "cart_items/destroy_all" => "cart_items#destroy_all"
     resources :cart_items, only: [:index, :update, :destroy, :create]
-    get "cart_items/destroy_all" => "cart_items#destroy_all"
     resources :orders, only: [:new, :create, :index, :show]
     post "orders/confirm" => "orders#confirm"
     get "orders/complete" => "orders#complete"
