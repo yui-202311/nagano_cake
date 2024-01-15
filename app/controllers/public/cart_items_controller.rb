f522f413b8226ebf3b01e576b6e99abd5cdc2a4d
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2024-01-15 09:58:53 +0000

    order.new

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,7 +7,7 @@ class Public::CartItemsController < ApplicationController
   def update
       @cart_items = CartItem.find(params[:id])
     if @cart_items.update(cart_item_params)
-      redirect_to public_cart_item_path
+      redirect_to public_cart_items_path
     else
       @cart_items = CartItem.find(params[:id])
       render :index
@@ -28,11 +28,17 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    if @cart_item.save
-      redirect_to public_cart_items_path
-    else
-      render :show
-    end
+    @cart_items = current_customer.cart_items.all
+   if current_customer.cart_items.exists?(item_id: @cart_item.item_id)
+    @cart_item = current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+    @cart_item.amount += params[:cart_item][:amount].to_i
+    @cart_item.save
+    redirect_to public_cart_items_path
+  elsif @cart_item.save
+    redirect_to public_cart_items_path
+  else
+    render :show
+  end
   end
 
   def cart_item_params
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
@@ -11,7 +11,10 @@ class Customer < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :telephone_number, presence: true
-  
-  has_many :cart_items
-    
+
+  has_one_attached :image
+  # scope :price_high_to_low, -> { order(price: :desc) }
+  # scope :price_low_to_high, -> { order(price: :asc) }
+  has_many :cart_items, dependent: :destroy
+
 end
